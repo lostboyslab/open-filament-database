5a86d1a197c68ae1dbf593b6d7cd553f75b14e39
Frida Rosenaa <macrosedk@gmail.com>
2025-07-30 17:08:02 +0200

    Fix redirect brand issue

--- webui/src/routes/+page.server.ts
+++ webui/src/routes/+page.server.ts
@@ -1,4 +1,4 @@
-import { createBrand } from '$lib/server/helpers';
+import { createBrand, stripOfIllegalChars } from '$lib/server/helpers';
 import { brandSchema } from '$lib/validation/filament-brand-schema';
 import { fail } from '@sveltejs/kit';
 import { superValidate } from 'sveltekit-superforms';
@@ -47,6 +47,6 @@ export const actions = {
       return fail(500, { form: sanitizedForm });
     }
 
-    redirect(form.data.brand, { type: 'success', message: 'Brand created successfully!' }, cookies);
+    redirect(stripOfIllegalChars(form.data.brand), { type: 'success', message: 'Brand created successfully!' }, cookies);
   },
 };
